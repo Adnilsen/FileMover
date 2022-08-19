bf63aae2f5a1300f237c149871ea3aeee5630231
adnilsen <adrian.bn@hotmail.com>
2022-08-19 16:23:23 +0200

    Initial

---
+++ API/Controllers/HubsController.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HubsController : ControllerBase
+    {
+        private readonly ILogger<HubsController> _logger;
+        private readonly ForgeService _forgeService;
+
+        public HubsController(ILogger<HubsController> logger, ForgeService forgeService)
+        {
+            _logger = logger;
+            _forgeService = forgeService;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<string>> ListHubs()
+        {
+            var tokens = await AuthController.PrepareTokens(Request, Response, _forgeService);
+            if (tokens == null)
+            {
+                return Unauthorized();
+            }
+            var hubs = await _forgeService.GetHubs(tokens);
+            return JsonConvert.SerializeObject(hubs);
+        }
+
+        [HttpGet("{hub}/projects")]
+        public async Task<ActionResult<string>> ListProjects(string hub)
+        {
+            var tokens = await AuthController.PrepareTokens(Request, Response, _forgeService);
+            if (tokens == null)
+            {
+                return Unauthorized();
+            }
+            var projects = await _forgeService.GetProjects(hub, tokens);
+            return JsonConvert.SerializeObject(projects);
+        }
+
+        [HttpGet("{hub}/projects/{project}/contents")]
+        public async Task<ActionResult<string>> ListItems(string hub, string project, [FromQuery] string? folder_id)
+        {
+            var tokens = await AuthController.PrepareTokens(Request, Response, _forgeService);
+            if (tokens == null)
+            {
+                return Unauthorized();
+            }
+            var contents = await _forgeService.GetContents(hub, project, folder_id, tokens);
+            return JsonConvert.SerializeObject(contents);
+        }
+
+        [HttpGet("{hub}/projects/{project}/contents/{item}/versions")]
+        public async Task<ActionResult<string>> ListVersions(string hub, string project, string item)
+        {
+            var tokens = await AuthController.PrepareTokens(Request, Response, _forgeService);
+            if (tokens == null)
+            {
+                return Unauthorized();
+            }
+            var versions = await _forgeService.GetVersions(hub, project, item, tokens);
+            return JsonConvert.SerializeObject(versions);
+        }
+    }
+}
---
+++ API/Models/ForgeService.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autodesk.Forge;
+
+namespace API.Models
+{
+   public class Tokens
+{
+    public string InternalToken;
+    public string PublicToken;
+    public string RefreshToken;
+    public DateTime ExpiresAt;
+}
+
+public partial class ForgeService
+{
+    private readonly string _clientId;
+    private readonly string _clientSecret;
+    private readonly string _callbackUri;
+    private readonly Scope[] InternalTokenScopes = new Scope[] { Scope.DataRead, Scope.ViewablesRead };
+    private readonly Scope[] PublicTokenScopes = new Scope[] { Scope.ViewablesRead };
+
+    public ForgeService(string clientId, string clientSecret, string callbackUri)
+    {
+        _clientId = clientId;
+        _clientSecret = clientSecret;
+        _callbackUri = callbackUri;
+    }
+}
+}
